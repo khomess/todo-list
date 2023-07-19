b95412fbb550a4648bf3d7d1a5db1cbe5a3ee861
Oleksandr Khomenko <sasha.khomenko12@gmail.com>
2023-07-19 17:09:13 +0300

    initial commit

---
+++ src/test/java/com/my/springboot/myfirstwebapp/MyfirstwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.springboot.myfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
